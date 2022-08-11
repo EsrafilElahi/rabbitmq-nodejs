b978f915f92b17e393e3db9a7cf239d05923934c
Esrafil <57289402+EsrafilElahi@users.noreply.github.com>
2022-08-11 23:45:20 +0430

    add files

---
+++ consumer.js
@@ -0,0 +1,50 @@
+const amqplib = require("amqplib/callback_api");
+
+// 1 - connect to amqp
+// 2 - create channel
+// 3 - receive msg from queue
+
+// const connectProvider = () => {
+//   return amqplib.connect("amqp://localhost", (err, connection) => {
+//     if (err) return;
+//     connection.createChannel((err, channel) => {
+//       const QUEUE = "test";
+
+//       channel.consume(
+//         QUEUE,
+//         (msg) => {
+//           console.log(`receive msg from queue ${QUEUE}`);
+//           console.log(`msg : ${msg.content.toString()}`);
+//         },
+//         { noAck: true }
+//       );
+//     });
+//   });
+// };
+
+// connectProvider();
+
+var amqp_url = process.env.CLOUDAMQP_URL || "amqp://localhost:5672";
+
+async function do_consume() {
+  var connection = await amqplib.connect(amqp_url, "heartbeat=60");
+  var channel = await connection.createChannel();
+  var queue = "test_queue";
+  await connection.createChannel();
+  await channel.assertQueue(q, { durable: true });
+  await channel.consume(
+    queue,
+    function (msg) {
+      console.log(msg.content.toString());
+      channel.ack(msg);
+      channel.cancel("myconsumer");
+    },
+    { consumerTag: "myconsumer" }
+  );
+  setTimeout(function () {
+    channel.close();
+    connection.close();
+  }, 500);
+}
+
+do_consume();
---
+++ provider.js
@@ -0,0 +1,45 @@
+const amqplib = require("amqplib/callback_api");
+
+// 1 - connect to amqp
+// 2 - create channel
+// 3 - send msg to queue
+
+// const connectConsumer = () => {
+//   return amqplib.connect("amqp://localhost", (err, connection) => {
+//     if (err) return;
+
+//     connection.createChannel((err, channel) => {
+//       const QUEUE = "test";
+//       const MESSAGE = "this is test message";
+
+//       channel.assertQueue(QUEUE, { durable: false });
+//       channel.sendToQueue(QUEUE, Buffer.from(MESSAGE));
+//       console.log(`msg sent to queue ${QUEUE}`);
+//     });
+//   });
+// };
+
+// connectConsumer();
+
+const amqp_url = process.env.CLOUDAMQP_URL || "amqp://localhost:5672";
+
+async function produce() {
+  console.log("Publishing");
+  const connection = await amqplib.connect(amqp_url, "heartbeat=60");
+  const channel = await connection.createChannel();
+  const exchange = "test_exchange";
+  const queue = "test_queue";
+  const routeKey = "test_route";
+  const msg = "Hello World!";
+  await channel
+    .assertExchange(exchange, "direct", { durable: true })
+    .catch(console.error);
+  await channel.assertQueue(queue, { durable: true });
+  await channel.bindQueue(queue, exchange, routeKey);
+  await channel.publish(exchange, routeKey, Buffer.from(msg));
+  setTimeout(function () {
+    channel.close();
+    connection.close();
+  }, 500);
+}
+produce();
